1265a1c6bc13fe7a05a505fa251a350adaa98148
vndriy <vndriy@users.noreply.github.com>
2023-10-01 11:08:47 +0200

    final push

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,6 +9,7 @@ const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]')
 
+start.disabled = true;
 
 const options = {
   enableTime: true,
@@ -17,14 +18,58 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      alert('Please choose a date in the future') 
+    } else {
+      start.disabled = false;
+    }
   },
 };
 
+start.addEventListener('click', onStart)
 
+function onStart() {
+  start.disabled = true;
+  input.disabled = true;
 
+  intervalId = setInterval(() => {
 
-input.addEventListener("click", () => {
-  dateWindow.open()
-});
+      const currentDate = Date.now();
+  const selectedDate = new Date(input.value);
+  const difference = selectedDate - currentDate;
+  
+    const timer = convertMs(difference);
+
+    days.textContent = timer.days.toString().padStart(2, 0);
+    hours.textContent = timer.hours.toString().padStart(2, 0);
+    minutes.textContent = timer.minutes.toString().padStart(2, 0);
+    seconds.textContent = timer.seconds.toString().padStart(2, 0);
+
+    if (difference < 1000) {
+      clearInterval(intervalId);
+    }
+
+  }, 1000);
+}
 
 flatpickr(input, options);
+
+
+function convertMs(ms) {
+// Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+// Remaining days
+  const days = Math.floor(ms / day);
+// Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+// Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+// Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formDelay = Number(form.querySelector('[name="delay"]').value);
+  const step = Number(form.querySelector('[name="step"]').value);
+  const amount = Number(form.querySelector('[name="amount"]').value);
+
+  for (let i = 0; i < amount; i++) {
+    stepDelay = formDelay + step * i
+    createPromise(i+1, stepDelay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+      })
   }
-}
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => { 
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay })
+    } else {
+      reject({ position, delay })
+    }
+  }, delay)
+})
+  }
+
